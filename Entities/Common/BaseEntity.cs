f965a3b8ff398f361db867efd72055c0e6c9cedf
مهدی حاجیان <mahdi.haj.1375@gmail.com>
2021-03-28 12:24:15 +0430

    Add project files.

---
+++ Entities/Common/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Common
+{
+    public interface IEntity
+    {
+    }
+
+    public abstract class BaseEntity<TKey> : IEntity
+    {
+        public TKey Id { get; set; }
+    }
+}
